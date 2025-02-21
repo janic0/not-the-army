98d19cc09274e89888bb552f9a3b09a1fb3de703
janic0 <git@janic.io>
2025-02-21 07:46:31 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Not the army
+
+A quick heads-up to people mistying the army's domain that they've actually reached, well, not the army.
+
+Also includes a couple of security tips below.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'bg-primary': "#1D1D1D",
+        'bg-secondary': '#333333',
+        'fg-primary': '#FFFFFF',
+        'accent-primary': 'rgb(205, 14, 14)'
+      }
+    },
+  },
+  plugins: [],
+}
+
